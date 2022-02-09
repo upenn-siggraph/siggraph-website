13be562125d2be46c67ed2d3827c5eae4739d3c6
printer83mph <printer.83mph@gmail.com>
2022-02-09 12:54:24 -0500

    Revert "Add next-optimized-image"
    This reverts commit cac40f23f3042d70ffdaa54c19bbf3c3ef0a4fb6.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const { withPlugins } = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withPlugins([[optimizedImages, {
-  
-}]], nextConfig)
+module.exports = nextConfig
