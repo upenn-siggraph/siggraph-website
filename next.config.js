6141e9773f86773eb2889a45bcd67803e2d0ac4b
printer83mph <printer.83mph@gmail.com>
2022-02-09 13:02:06 -0500

    Add next lqip plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const withPlugins = require('next-compose-plugins')
+const withLqipImages = require('next-lqip-images')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([withLqipImages], nextConfig)
