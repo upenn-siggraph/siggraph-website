5c4b573d20b7c2990ac6a3defaccd086748d2b82
printer83mph <printer.83mph@gmail.com>
2022-02-09 23:42:00 -0500

    Clean

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-
-/** @type import("postcss").Postcss */
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    'postcss-preset-env': {},
-  },
-}
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom'
-import App from './app'
-
-import './global.css'
-
-ReactDOM.render(App(), document.querySelector('#root'))
