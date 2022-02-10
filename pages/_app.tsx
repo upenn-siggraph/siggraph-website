d96fc9d54c46a3074eafeaadf3dd5ce19726994e
printer83mph <printer.83mph@gmail.com>
2022-02-09 22:36:06 -0500

    Switch to layout effect for body bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react-layout-effect'
 
 import '../styles/global.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
+  useLayoutEffect(() => {
     document.body.classList.add('dark:bg-gray-100', 'dark:text-gray-900')
   }, [])
   return <Component {...pageProps} />
