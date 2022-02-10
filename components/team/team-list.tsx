6af879accf1f00bbe8b2e4e8b00abd8ec79024f9
printer83mph <printer.83mph@gmail.com>
2022-02-09 22:42:24 -0500

    Custom document and style fixes

--- components/team/team-list.tsx
+++ components/team/team-list.tsx
@@ -2,7 +2,7 @@ import team from '../../lib/team'
 import LazyImage from '../util/lazy-image'
 
 const TeamList = () => (
-  <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto">
+  <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
     {team.map(({ name, role, image }) => (
       <li key={name} className="mb-4">
         <LazyImage
@@ -10,7 +10,7 @@ const TeamList = () => (
           width={300}
           height={300}
           alt={name}
-          className="aspect-square w-auto h-auto rounded-xl shadow-lg"
+          className="aspect-square w-full h-auto rounded-xl shadow-lg"
         />
         <div className="m-4">
           <h3 className="text-xl font-bold dark:text-gray-900">{name}</h3>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import type { AppProps } from 'next/app'
-import { useLayoutEffect } from 'react-layout-effect'
 
 import '../styles/global.scss'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  useLayoutEffect(() => {
-    document.body.classList.add('dark:bg-gray-100', 'dark:text-gray-900')
-  }, [])
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
