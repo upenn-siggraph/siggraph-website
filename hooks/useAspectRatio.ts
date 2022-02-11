68773aca0091923aa7025bf3d45b518e71ffa59c
printer83mph <printer.83mph@gmail.com>
2022-02-11 17:47:17 -0500

    Add nav background

---
+++ hooks/useAspectRatio.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from 'react'
+
+const getRatio = () => {
+  if (window === undefined) return 1
+  return window.innerWidth / window.innerHeight
+}
+
+const useAspectRatio = () => {
+  const [ratio, setRatio] = useState<number>(1)
+
+  const resizeCallback = useCallback(() => {
+    setRatio(getRatio())
+  }, [])
+
+  useEffect(() => {
+    if (window === undefined) return () => {}
+    resizeCallback()
+    window.addEventListener('resize', resizeCallback)
+    return () => {
+      window.removeEventListener('resize', resizeCallback)
+    }
+  }, [resizeCallback])
+
+  return ratio
+}
+
+export default useAspectRatio
