604c2a26562cfc29cbd0b3cc200f1742abde3a2a
printer83mph <printer83mph@users.noreply.github.com>
2022-02-14 03:46:43 +0000

    deploy: 22bdd618036e6b5478f8c065cfdbcd0307eadec4

---
+++ _next/static/REdTEL0wc5-yiObvjMy8m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e17a2640a4da472a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/team":["static/chunks/pages/team-a0d9f8d4fc6a2701.js"],sortedPages:["/","/_app","/_error","/team"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
