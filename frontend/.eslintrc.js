4cf4d99a371038beb02d336e215b28b1465766bf
printer83mph <printer.83mph@gmail.com>
2022-02-17 18:33:01 -0500

    Move stuff to frontend folder

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  plugins: ['unused-imports'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __pathname,
+      },
+    },
+  ],
+  rules: {
+    'react/jsx-props-no-spreading': 0,
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+
+    '@next/next/no-img-element': 0,
+
+    'unused-imports/no-unused-imports': 'error',
+  },
+}
