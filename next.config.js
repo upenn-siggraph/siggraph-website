7a07fdfef0ce93b0450c7b9bb31c30bf80d87bc9
Thomas Shaw <printer.83mph@gmail.com>
2023-08-23 16:58:28 -0400

    Update config for static export

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 }
